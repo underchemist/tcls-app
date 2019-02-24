43e8b473409a73aff01add70810cd1276326c85e
sebastien tremblay <yanns.tremblay@gmail.com>
2019-02-23 18:55:48 -0800

    Add initial models

--- tcls/core/models.py
+++ tcls/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class LogFile(models.Model):
+    raw_data = models.FileField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ChatComment(models.Model):
+    time = models.DateTimeField(blank=True)
+    username = models.CharField(max_length=256, blank=True)
+    msg = models.CharField(max_length=512)  # twich chat max char count is apparently 500
+    logfile = models.ForeignKey(LogFile, on_delete=models.CASCADE)
