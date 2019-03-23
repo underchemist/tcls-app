6785fd21b57e15e79940d31b5f092eca99518de0
Yann-Sebastien Tremblay-Johnston <yanns.tremblay@gmail.com>
2019-03-22 23:31:05 -0700

    Add some methods for checking discrepency between chatlog duration and duration listed in video object from api

--- tcls/core/models.py
+++ tcls/core/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 from django.core.serializers.json import DjangoJSONEncoder
-from django.utils.dateparse import parse_datetime
+from django.utils.dateparse import parse_datetime, parse_duration
+from datetime import timedelta
 
 class Video(models.Model):
     data = JSONField(encoder=DjangoJSONEncoder)
@@ -16,11 +17,34 @@ class Video(models.Model):
 
 
 class LogFile(models.Model):
+    video = models.OneToOneField(Video, on_delete=models.CASCADE)
     filepath = models.FileField(max_length=200)
-    video_id = models.CharField(max_length=20, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def _tail(self, offset=-1024):
+        """Determine the approximate duration of the chatlog by tailing the end of the logfile """
+        with self.filepath.open('rb') as f:
+            f.seek(offset, 2)  # seek to almost EOF
+            tail = [line.decode('utf-8') for line in f.readlines()]
+
+            return tail
+
+    def _calc_duration(self):
+        tail = self._tail()
+
+        duration_str = tail[-1].split(',')[0].strip('[]')
+        duration = parse_duration(duration_str)
+
+        return duration
+
+    def _is_similar_length(self,tol=timedelta(minutes=1)):
+        """Compare calculated duration of chatlog and the duration from video object in order to say whether the chatlog has actually downloaded the full amount
+        """
+        chatlog_duration = self._calc_duration()
+        video_duration = parse_duration(self.video.data['duration'].replace('m', ':').replace('h', ':').strip('s'))
+
+        return video_duration - chatlog_duration < tol
 
 class ChatComment(models.Model):
     id = models.BigAutoField(primary_key=True)
